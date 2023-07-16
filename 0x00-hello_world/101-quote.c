0be298612770d4f544d2ddb4bd32a4ea518b2d37
GiftFAC <hadisanusi2020@gmail.com>
2023-07-15 23:10:32 -0700

    fifth coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,17 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- *        - Dora Korpar, 2015-10-19"
- *        followed by a new line, to the standard error.
+ * main - prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
  *
- * Return: Always 1
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	return (1);
+    ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+    ssize_t bytes_written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+
+    if (bytes_written != len)
+        return 1;
+    return 1;
 }
